f5cb06681154e8ebecaf78ed2dba52a42c61fb3b
Dev-kris <corpkris@gmail.com>
2020-05-12 18:37:20 +0200

    Added validation attributes to ProductViewModel.

--- P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
+
+    // Added attributes to validate input
+    // Set validation for int fields
     public class ProductViewModel
     {
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingName")]
         public string Name { get; set; }
 
+        
         public string Description { get; set; }
 
+        
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingStock"), Range(1, int.MaxValue, ErrorMessage = "Stock must be a valid number")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingPrice"), Range(1, int.MaxValue, ErrorMessage = "Price must be a valid number")]
         public string Price { get; set; }
     }
 }
